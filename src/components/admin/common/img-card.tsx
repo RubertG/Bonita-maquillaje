7b2e9b0efee08f4e0c6ea367f282ca82397566c6
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-24 18:03:16 -0500

    🆕 Cards de las imagenes con funcionalidad de drag

--- src/components/admin/common/img-card.tsx
+++ src/components/admin/common/img-card.tsx
@@ -1,30 +1,27 @@
 "use client"
 
 import { Delete } from "@/components/common/icons"
-import { FileStateItem } from "@/types/admin/admin"
 import { returnFileSize } from "@/utils/return-file-size"
-import { useState } from "react"
-import { PopupDelete } from "./popup-delete"
 import { useSortable } from "@dnd-kit/sortable"
 import { CSS } from "@dnd-kit/utilities"
-export const OldImg = ({
-  item,
-  handleDeleteOld,
-  aspect
-}: {
-  item: FileStateItem
-  handleDeleteOld: (item: FileStateItem) => Promise<void>
+
+interface Props {
+  item: File
+  handleDelete: (item: File) => void
   aspect: string
-}) => {
-  const [popup, setPopup] = useState(false)
-  const [loading, setLoading] = useState(false)
+}
+
+export const ImgCard = ({
+  item,
+  handleDelete,
+  aspect
+}: Props) => {
   const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: `${item.name}-${item.size}` })
 
   const style = {
     transform: CSS.Transform.toString(transform),
     transition
   }
-  const handlePopup = () => setPopup(!popup)
 
   return (
     <li className="flex w-full gap-2 items-center rounded-lg justify-between lg:transition-colors overflow-hidden">
@@ -40,13 +37,13 @@ export const OldImg = ({
             style={{ aspectRatio: aspect }}
             className="w-16 object-cover rounded-lg"
             loading="lazy"
-            src={item.url} alt={`${item.name} - Bonita Maquillaje`}
+            src={URL.createObjectURL(item)} alt={`${item.name} - Bonita Maquillaje`}
             title={`${item.name} - Bonita Maquillaje`} />
           <div className="w-full overflow-hidden">
             <p className="font-light text-text-200 whitespace-nowrap overflow-hidden text-ellipsis"
               title={item.name}
             >
-              {item.name} dawwwwwwwwwwwwwwwww
+              {item.name}
             </p>
             <p className="font-light text-sm text-text-200">
               {returnFileSize(item.size)}
@@ -55,24 +52,10 @@ export const OldImg = ({
         </div>
       </div>
       <button
-        onClick={handlePopup}
+        onClick={() => handleDelete(item)}
       >
         <Delete className="stroke-text-300 lg:hover:scale-110 lg:transition-transform" />
       </button>
-      {
-        popup &&
-        <PopupDelete
-          title="¿Deseas eliminar esta imagen?"
-          handleDelete={async () => {
-            setLoading(true)
-            await handleDeleteOld(item)
-            setLoading(false)
-            setPopup(false)
-          }}
-          handlePopup={handlePopup}
-          loading={loading}
-        />
-      }
     </li>
   )
 }
