45703e0887fd41d2b7f2f040fd56749e8630f5bd
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-25 09:57:59 -0500

    🛠️ Sensor para movil

--- src/components/admin/common/images-container.tsx
+++ src/components/admin/common/images-container.tsx
@@ -3,8 +3,8 @@
 import { FileStateItem } from "@/types/admin/admin"
 import { ImgCard } from "./img-card"
 import { OldImg } from "./old-img"
-import { DndContext, DragEndEvent, closestCenter } from "@dnd-kit/core"
-import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable"
+import { DndContext, DragEndEvent, KeyboardSensor, MouseSensor, TouchSensor, closestCenter, useSensor, useSensors } from "@dnd-kit/core"
+import { SortableContext, sortableKeyboardCoordinates, verticalListSortingStrategy } from "@dnd-kit/sortable"
 
 interface Props {
   className?: string
@@ -20,11 +20,32 @@ export const ImagesContainer = ({
   handleDragEnd, handleDelete,
   aspect, handleDeleteOld
 }: Props) => {
+  const sensors = useSensors(
+    useSensor(MouseSensor, {
+      // Require the mouse to move by 10 pixels before activating
+      activationConstraint: {
+        distance: 10
+      }
+    }),
+    useSensor(TouchSensor, {
+      // Press delay of 250ms, with tolerance of 5px of movement
+      activationConstraint: {
+        delay: 250,
+        tolerance: 5
+      }
+    }),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates
+    })
+  )
+
+
   return (
     <ul className={`grid gap-3 ${className}`}>
       <DndContext
         collisionDetection={closestCenter}
         onDragEnd={handleDragEnd}
+        sensors={sensors}
       >
         <SortableContext
           items={images.map(item => `${item.name}-${item.size}`)}
