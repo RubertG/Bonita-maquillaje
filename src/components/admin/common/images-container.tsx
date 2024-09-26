4558b8cebedffc22dbbaf21c4e0e96bbbff7f869
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-25 19:49:42 -0500

    🆑 Comentarios

--- src/components/admin/common/images-container.tsx
+++ src/components/admin/common/images-container.tsx
@@ -22,13 +22,11 @@ export const ImagesContainer = ({
 }: Props) => {
   const sensors = useSensors(
     useSensor(MouseSensor, {
-      // Require the mouse to move by 10 pixels before activating
       activationConstraint: {
         distance: 10
       }
     }),
     useSensor(TouchSensor, {
-      // Press delay of 250ms, with tolerance of 5px of movement
       activationConstraint: {
         delay: 250,
         tolerance: 5
