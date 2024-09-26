b728efd332e073f49d40c39f69debdb6936f8bcf
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-25 19:50:15 -0500

    🛠️ Cargar la id de la categoria

--- src/app/(initialize-firebase)/admin/(protected-routes)/productos/crear-producto/page.tsx
+++ src/app/(initialize-firebase)/admin/(protected-routes)/productos/crear-producto/page.tsx
@@ -1,12 +1,20 @@
 import { CreateProductForm } from "@/components/admin/products/create-product-form"
+import { BackButtonCategory } from "@/components/catalogue/back-button-category"
 import { BackButton } from "@/components/common/back-button"
 import { H1 } from "@/components/common/h1"
+import { Suspense } from "react"
 
 export default function CreateProductPage() {
   return (
     <main className="px-4 my-16 xl:px-0 lg:mt-20 max-w-5xl mx-auto">
-      <BackButton
-        href="/admin/productos" />
+      <Suspense fallback={(
+        <BackButton
+          href="/admin/productos" />
+      )}>
+        <BackButtonCategory
+          href="/admin/productos"
+        />
+      </Suspense>
       <H1 className="mb-6 mt-2">Crear producto</H1>
       <CreateProductForm />
     </main>
