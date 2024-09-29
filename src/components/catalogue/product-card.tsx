001f0806c004f80e7eadcc0ea6c5d9979f023e56
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-29 12:44:09 -0500

    🛠️ Imagenes de los productos del catalogo

--- src/components/catalogue/product-card.tsx
+++ src/components/catalogue/product-card.tsx
@@ -1,16 +1,18 @@
 import { Product } from "@/types/db/db"
+import Image from "next/image"
 import Link from "next/link"
 
 export const ProductCard = ({ name, price, imgs, id }: Product) => {
   return (
     <Link
       href={`/catalogo/${id}`}
-      className="lg:p-2.5 rounded-lg lg:hover:bg-bg-200 cursor-pointer lg:transition-colors">
-      <img
-        className="w-full aspect-[3/4] object-cover rounded-lg"
+      className="lg:p-2.5 rounded-lg lg:hover:bg-bg-200 lg:hover:scale-105 cursor-pointer lg:transition-all">
+      <Image
+        width={200}
+        height={200 * (3 / 4)}
+        className="w-full aspect-[3/4] object-cover rounded-lg bg-bg-200"
         src={imgs[0].url}
         alt={`${name} - Bonita Maquillaje`}
-        loading="lazy"
       />
       <footer className="mt-2 text-center">
         <h2 className="text-text-100 line-clamp-2">
