7aba406d914bf8cf4983e2e032de91cacbc80095
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-11-21 14:22:27 -0500

    🆕 Estilos del numero de items del carrito

--- src/components/catalogue/cart-button.tsx
+++ src/components/catalogue/cart-button.tsx
@@ -1,24 +1,32 @@
 "use client"
 
-import Link from 'next/link'
 import { Cart } from '../common/icons'
 import { useEffect, useState } from 'react'
 import { getCart } from '@/utils/cart'
+import { useRouter } from 'next/navigation'
 
 export const CartButton = () => {
   const [cartSize, setCartSize] = useState(0)
+  const router = useRouter()
 
   useEffect(() => {
     getCart().then(cart => setCartSize(cart.length)).catch(() => setCartSize(0))
   }, [])
 
   return (
-    <>
-      {cartSize}
-      <Link href="/carrito" title="Ir al carrito">
-        <Cart className="stroke-text-100 lg:transition-all lg:hover:stroke-accent-300 lg:hover:scale-110" />
-      </Link>
-    </>
+    <button
+      onClick={() => router.push("/carrito")}
+      title="Ir al carrito"
+      className='relative block'
+    >
+      {
+        cartSize > 0 &&
+        <p className='absolute -top-2 -right-2.5 bg-red-500 text-xs w-5 h-5 pt-[1px] font-medium rounded-full flex text-white justify-center items-center z-10'>
+          {cartSize > 99 ? "99" : cartSize}
+        </p>
+      }
+      <Cart className="stroke-text-100 lg:transition-all lg:hover:stroke-accent-300 lg:hover:scale-110" />
+    </button>
   )
 }
 
