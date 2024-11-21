7838f838707d4b0702c6d6f15cdc9026887c33ab
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-11-21 14:53:44 -0500

    🛠️ manejador de estado del carrito

---
+++ src/stores/cart/cart.store.ts
@@ -0,0 +1,61 @@
+import { getProduct } from "@/firebase/services/products"
+import { Product } from "@/types/admin/admin"
+import { ItemCart } from "@/types/catalogue/cart"
+import { create, StateCreator } from "zustand"
+import { persist } from "zustand/middleware"
+
+interface CartState {
+  items: ItemCart[]
+
+  addItem: (item: ItemCart) => void
+  updateCart: (cart: Product[]) => void
+  changeCount: (id: string, count: number) => void
+  getCart: () => Promise<Product[]>
+}
+
+const cartStore: StateCreator<CartState> = (set, get) => ({
+  items: [],
+
+  addItem: (item) => set({ items: [...get().items, item] }),
+  updateCart: (p) => {
+    const newCart: ItemCart[] = p.map((product) => ({
+      id: product.id,
+      color: product.tone?.color,
+      amount: product.amount
+    }))
+
+    set({ items: newCart })
+  },
+  changeCount: (id, count) => set({ items: get().items.map(it => it.id === id ? { ...it, amount: count } : it) }),
+
+  getCart: async () => {
+    const cartLocal = get().items
+
+    const productsCart = await Promise.all(cartLocal.map(async (item) => {
+      const product = await getProduct(item.id)
+
+      if (!product) return null
+
+      const tone = product.tones.find(t => t.color === item.color) || product.tones[0]
+
+      const newProduct: Product = {
+        ...product,
+        amount: item.amount,
+        tone
+      }
+
+      return newProduct
+    }))
+
+    return productsCart.filter(product => product !== null)
+  }
+})
+
+export const useCartStore = create<CartState>()(
+  persist(
+    cartStore,
+    {
+      name: 'cart'
+    }
+  )
+)
