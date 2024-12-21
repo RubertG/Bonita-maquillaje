443183a4e36a5a8a7119c8a00caa871be2debfb7
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-12-21 10:23:42 -0500

    🛠️ Actualización del README y adición de dependencias para transiciones de vista

--- src/app/(initialize-firebase)/(nav-client)/layout.tsx
+++ src/app/(initialize-firebase)/(nav-client)/layout.tsx
@@ -1,10 +1,11 @@
 import { Nav } from "@/components/catalogue/nav"
+import { ViewTransitions } from "next-view-transitions"
 
 export default function CatalogueLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <ViewTransitions>
       <Nav />
       {children}
-    </>
+    </ViewTransitions>
   )
 }
