ad1a41bda61a0c19d7c30173140a18bfd3855087
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-12-21 13:31:03 -0500

    🛠️ Revertir el uso de ViewTransitions en el layout del catálogo

--- src/app/(initialize-firebase)/(nav-client)/layout.tsx
+++ src/app/(initialize-firebase)/(nav-client)/layout.tsx
@@ -1,11 +1,10 @@
 import { Nav } from "@/components/catalogue/nav"
-import { ViewTransitions } from "next-view-transitions"
 
 export default function CatalogueLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ViewTransitions>
+    <>
       <Nav />
       {children}
-    </ViewTransitions>
+    </>
   )
 }
